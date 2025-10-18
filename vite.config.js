a56649bfa8d8baa36d13abaa3ed84f378d96d06c
ALSemenov <alhaos@gmail.com>
2025-10-18 15:50:43 +0500

    pwa display mode changed to fullscreen

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
             short_name: 'Measurements',
             description: 'alhaos measurements',
             theme_color: '#ffffff',
+            display: "fullscreen",
         },
 
         workbox: {
