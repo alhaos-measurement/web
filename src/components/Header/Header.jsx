8981220b95a5f6f13dc7b3ea115f210045192ba7
ALSemenov <alhaos@gmail.com>
2025-10-18 15:54:49 +0500

    city added to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ function Header() {
 
     return <div className={styles.container}>
         <div className={styles.caption}>АТМОСФЕРНОЕ ДАВЛЕНИЕ</div>
+        <div className={styles.caption}>В Пыть-Яхе</div>
     </div>
 
 }
